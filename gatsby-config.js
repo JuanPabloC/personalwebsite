3ffff5a9a1568b15ab97bb9c18e0b32701c0b80d
Juan Pablo Castano <jpcastanom@gmail.com>
2020-12-11 16:16:08 -0500

    Updated metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Juan Pablo Castano`,
+    description: `Juan Pablo's Personal Website`,
+    author: `@JuanPabloC`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
