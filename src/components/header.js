dc9661e6f3b37c786f0ae97a1229f6f3fa0a5608
Juan Pablo Castano <jpcastanom@gmail.com>
2020-12-12 16:16:39 -0500

    Moved style to custom.css and hid list

--- src/components/header.js
+++ src/components/header.js
@@ -3,49 +3,20 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle, menuLinks }) => (
-  <header
-    style={{
-      background: `rgba(220,220,220,0.5)`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        padding: `1.45rem 3.0rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `black`,
-            textDecoration: `none`,
-          }}
+  <header>
+    <h1 style={{ margin: 0 }}> Juan Pablo Castano</h1>
+    {/* <ul className="nav">
+      {menuLinks.map(link => (
+        <li
+          key={link.name}
+          className="navItem"
         >
-          Juan Pablo Castano
-        </Link>
-      </h1>
-      {/* <div>
-          <nav>
-            <ul style={{ display: "flex", flex: 1 }}>
-              {menuLinks.map(link => (
-                <li
-                  key={link.name}
-                  style={{
-                    listStyleType: `none`,
-                    padding: `1rem`,
-                  }}
-                >
-                  <Link style={{ color: `black` }} to={link.link}>
-                    {link.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div> */}
-    </div>
+          <Link style={{ color: `black` }} to={link.link}>
+            {link.name}
+          </Link>
+        </li>
+      ))}
+    </ul> */}
   </header>
 )
 
