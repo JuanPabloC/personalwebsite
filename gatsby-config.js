c00f9777efabc20fb8884ad685b474174001f20f
Juan Pablo Castano <jpcastanom@gmail.com>
2020-12-12 19:09:45 -0500

    Added Font plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,9 +40,20 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+      
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Roboto Slab\:300`,
+          `Roboto\:300`
+        ],
+        display: 'swap'
+      }
+    }
   ],
 }
