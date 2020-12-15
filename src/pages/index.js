c2aaf2bfd74cfac566b8399c5792fd0b5d197e8b
Juan Pablo Castano <jpcastanom@gmail.com>
2020-12-14 20:28:39 -0500

    Cropped front image banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,28 +6,31 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = (props) => (
-  <Layout>
-    <SEO title="Home" />
-    <BackgroundImage
-      className= "frontBanner"
-      fluid={props.data.indexImage.childImageSharp.fluid}
-    >
-      <div
-        className="blackOverlay"
+const IndexPage = (props) => {
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <BackgroundImage
+        className= "frontBanner"
+        fluid={props.data.indexImage.childImageSharp.fluid}
       >
-        <h1 className="bannerText">Hi, Welcome!</h1>
-        <h1 className="bannerText">My name is Juan Pablo</h1>
-      </div>
-    </BackgroundImage>
-  </Layout>
-)
+        <div
+          className="blackOverlay"
+        >
+          <h1 className="bannerText">Hi, Welcome!</h1>
+          <h1 className="bannerText">My name is Juan Pablo</h1>
+        </div>
+      </BackgroundImage>
+    </Layout>
+  )
+}
 
 export default IndexPage
 
 export const pageQuery = graphql`
   query {
-    indexImage: file(relativePath: {eq: "MainPhoto.JPG"}) {
+    indexImage: file(relativePath: {eq: "MainPhoto.jpg"}) {
       childImageSharp {
         fluid(maxWidth: 1800) {
           ...GatsbyImageSharpFluid
