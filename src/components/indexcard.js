9cffbcb224ee86a108979a7a460b46a93c9fe76c
Juan Pablo Castano <jpcastanom@gmail.com>
2020-12-16 13:48:54 -0500

    rounded indexcard borders and made off-white

--- src/components/indexcard.js
+++ src/components/indexcard.js
@@ -14,6 +14,8 @@ const Card =  styled("div")`
     margin: 30px;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     padding: 50px;
+    background: var(--off-white);
+    border-radius: 20px;
 `;
 
 
