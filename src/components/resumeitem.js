ee6487ab89f4a8ba0975e2a0a5d41d97240ae7aa
Juan Pablo Castano <jpcastanom@gmail.com>
2021-01-14 22:03:36 -0500

    Added collapsable roles

--- src/components/resumeitem.js
+++ src/components/resumeitem.js
@@ -1,10 +1,11 @@
 import React from "react"
 import styled from "@emotion/styled"
-import { Col, Row } from "antd"
+import { Col, Row, Collapse } from "antd"
 import Img from "gatsby-image"
 import { css } from "@emotion/react";
 import PropTypes from "prop-types"
 
+const { Panel } = Collapse;
 
 const ImgBox = styled("div")`
     width: 70%;
@@ -37,15 +38,14 @@ const ResumeItemDate = styled("h4")`
     font-weight: bold;
 `;
 
-const ResumeItemDescription = styled("p")``;
-
 const bottomMargin = css`
     margin-bottom: 20px;
 `;
 
+
 const ResumeItem = (props) => {
 
-    console.log(JSON.stringify(props))
+    console.log(JSON.stringify(props.org.roles))
 
     return (
         <Row css={bottomMargin}>
@@ -59,7 +59,12 @@ const ResumeItem = (props) => {
                     <ResumeItemTitle>{props.org.name}</ResumeItemTitle>
                     <ResumeItemLocation>{props.org.location}</ResumeItemLocation>
                 </ResumeItemHeading>
-                <ResumeItemDescription>{props.org.description}</ResumeItemDescription>
+                <p>{props.org.description}</p>
+                <Collapse>
+                    {props.org.roles.map((role,index) => (
+                        <Panel header={role} key={index+1} forceRender="true"></Panel>
+                    ))}
+                </Collapse>
             </Col>
             <Col span={6}>
                 <ResumeItemDate>{props.org.date}</ResumeItemDate>
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -89,26 +89,42 @@ let resumeJobs = {
     name: "Code Climate",
     location: "New York, NY",
     date: "Sept. 2018 - Oct. 2020",
-    description: "Code Climate’s flagship product, Velocity, analyzes all the data from your GitHub repos and provides you with heads-up displays, real-time analytics, and custom reports to give you a clearer perspective on how your engineering team is working."
+    description: "Code Climate’s flagship product, Velocity, analyzes all the data from your GitHub repos and provides you with heads-up displays, real-time analytics, and custom reports to give you a clearer perspective on how your engineering team is working.",
+    roles: [
+      "Outbound Business Development Representative",
+      "Associate Account Executive",
+      "Sales Development Representative"
+    ]
   },
   geometrica: {
     name: "Geometrica Inc",
     location: "Monterrey, Mexico",
     date: "June 2017 - Aug. 2017",
-    description: "Geometrica has designed, manufactured and installed domes and space frames since 1992. The company has developed unique technology to build amazing, long span structures for architectural and industrial buildings. With offices in Houston, Texas and Monterrey, Mexico, and a global network of representatives, Geometrica has delivered domes and space frames in more than 40 countries in North and South America, Europe, Asia and Africa."
+    description: "Geometrica has designed, manufactured and installed domes and space frames since 1992. The company has developed unique technology to build amazing, long span structures for architectural and industrial buildings. With offices in Houston, Texas and Monterrey, Mexico, and a global network of representatives, Geometrica has delivered domes and space frames in more than 40 countries in North and South America, Europe, Asia and Africa.",
+    roles: [
+      "Software Engineering Intern"
+    ]
   },
   uofr: {
     name: "University of Rochester",
     location: "Rochester, NY",
     date: "Aug. 2015 - May 2018",
-    description: "The University of Rochester (UR) is a private, nonsectarian, research-intensive university located in Rochester, New York. The University grants bachelor's, masters, and doctoral degrees through seven schools and various interdisciplinary programs.The University enrolls over 6,000 undergraduates and nearly 5,000 graduate students, and competes in the NCAA's Division III for athletics. The University with its affiliated UR Medicine health system is the largest employer in the Greater Rochester area."
-  },
+    description: "The University of Rochester (UR) is a private, nonsectarian, research-intensive university located in Rochester, New York. The University grants bachelor's, masters, and doctoral degrees through seven schools and various interdisciplinary programs.The University enrolls over 6,000 undergraduates and nearly 5,000 graduate students, and competes in the NCAA's Division III for athletics. The University with its affiliated UR Medicine health system is the largest employer in the Greater Rochester area.",
+    roles: [
+      "Peer Advisor - Computer Science Department",
+      "Student Worker - Hajim School of Engineering Dean's Office",
+      "Teacher Assistant - Computer Science Department",
+      "Resident Advisor - Residential Life"
+    ]
+  }
 }
 
 let education = {
   uofr: {
     name: "University of Rochester",
     location: "Rochester, NY",
-    date: "Aug. 2014 - May 2018"
+    date: "Aug. 2014 - May 2018",
+    description: "",
+    roles: []
   }
 }
